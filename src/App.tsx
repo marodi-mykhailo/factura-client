c9681a5e2f771a8e0bda003d334832e9ccf181b6
marodi-mykhailo <mykhailo.marodi@gmail.com>
2020-11-25 19:37:06 +0100

    add material ui wrapper and table

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Main from "./Components/Main/Main";
+import DataTable from "./Components/Table/Table";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Main>
+                <DataTable/>
+            </Main>
+        </div>
+    );
 }
 
 export default App;
