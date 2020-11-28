4fd94273c3df45fdaa40ca2a64f966feb6cc18ff
marodi-mykhailo <mykhailo.marodi@gmail.com>
2020-11-28 13:06:14 +0100

    add validation to form

--- src/Components/InvoiceForm/InvoiceForm.tsx
+++ src/Components/InvoiceForm/InvoiceForm.tsx
@@ -14,9 +14,18 @@ import {KeyboardDatePicker, MuiPickersUtilsProvider} from "@material-ui/pickers"
 import DateFnsUtils from "@date-io/date-fns";
 
 const validationSchema = yup.object({
-    email: yup
+    client: yup
         .string()
-        .required('Email is required'),
+        .required('Client is required'),
+    seller: yup
+        .string()
+        .required('Seller is required'),
+    product: yup
+        .string()
+        .required('Product is required'),
+    status: yup
+        .string()
+        .required('Status is required'),
 });
 
 const useStyles = makeStyles((theme: Theme) =>
@@ -47,6 +56,7 @@ export const InvoiceForm = () => {
             product: '',
             status: ''
         },
+        validationSchema: validationSchema,
         onSubmit: (values) => {
             alert(JSON.stringify(values))
         },
@@ -96,105 +106,113 @@ export const InvoiceForm = () => {
     return (
         <Paper elevation={4} className={s.paper}>
             <form className={classes.root} onSubmit={formik.handleSubmit}>
-                    <TextField
-                        id="client"
-                        name="client"
-                        select
-                        label="Client"
-                        value={formik.values.client}
-                        onChange={formik.handleChange}
-                        helperText="Please select your Client"
-                        variant="outlined"
+                <TextField
+                    id="client"
+                    name="client"
+                    select
+                    label="Client"
+                    value={formik.values.client}
+                    onChange={formik.handleChange}
+                    placeholder="Please select your Client"
+                    variant="outlined"
+                    className={s.input}
+                    error={formik.touched.client && Boolean(formik.errors.client)}
+                    helperText={formik.touched.client && formik.errors.client}
+                >
+                    {clients.map((option, idx) => (
+                        <MenuItem key={idx} value={option.ID}>
+                            {option.info.name}
+                        </MenuItem>
+                    ))}
+                </TextField>
+                <TextField
+                    id="seller"
+                    name="seller"
+                    select
+                    label="Seller"
+                    value={formik.values.seller}
+                    onChange={formik.handleChange}
+                    placeholder="Please select your Seller"
+                    variant="outlined"
+                    className={s.input}
+                    error={formik.touched.seller && Boolean(formik.errors.seller)}
+                    helperText={formik.touched.seller && formik.errors.seller}
+                >
+                    {sellers.map((option, idx) => (
+                        <MenuItem key={idx} value={option.ID}>
+                            {option.info.name}
+                        </MenuItem>
+                    ))}
+                </TextField>
+                <TextField
+                    id="product"
+                    name="product"
+                    select
+                    label="Product"
+                    value={formik.values.product}
+                    onChange={formik.handleChange}
+                    placeholder="Please select your Product"
+                    variant="outlined"
+                    className={s.input}
+                    error={formik.touched.product && Boolean(formik.errors.product)}
+                    helperText={formik.touched.product && formik.errors.product}
+                >
+                    {products[0]?.sellerProducts.map((option, idx) => (
+                        <MenuItem key={idx} value={option.ID}>
+                            {option.name}
+                        </MenuItem>
+                    ))}
+                </TextField>
+                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                    <KeyboardDatePicker
+                        margin="normal"
+                        id="paymentDate"
+                        name="paymentDate"
+                        label="Choose payment date"
+                        format="MM/dd/yyyy"
+                        value={paymentDate}
+                        onChange={paymentDateChange}
+                        KeyboardButtonProps={{
+                            'aria-label': 'change date',
+                        }}
                         className={s.input}
-                    >
-                        {clients.map((option, idx) => (
-                            <MenuItem key={idx} value={option.ID}>
-                                {option.info.name}
-                            </MenuItem>
-                        ))}
-                    </TextField>
-                    <TextField
-                        id="seller"
-                        name="seller"
-                        select
-                        label="Seller"
-                        value={formik.values.seller}
-                        onChange={formik.handleChange}
-                        helperText="Please select your Seller"
-                        variant="outlined"
+                    />
+                    <KeyboardDatePicker
+                        margin="normal"
+                        id="issueDate"
+                        name="issueDate"
+                        label="Choose issue date"
+                        format="MM/dd/yyyy"
+                        value={issueDate}
+                        onChange={issueDateChange}
+                        KeyboardButtonProps={{
+                            'aria-label': 'change date',
+                        }}
                         className={s.input}
-                    >
-                        {sellers.map((option, idx) => (
-                            <MenuItem key={idx} value={option.ID}>
-                                {option.info.name}
-                            </MenuItem>
-                        ))}
-                    </TextField>
-                    <TextField
-                        id="product"
-                        name="product"
-                        select
-                        label="Product"
-                        value={formik.values.product}
-                        onChange={formik.handleChange}
-                        helperText="Please select your Product"
-                        variant="outlined"
-                        className={s.input}
-                    >
-                        {products[0]?.sellerProducts.map((option, idx) => (
-                            <MenuItem key={idx} value={option.ID}>
-                                {option.name}
-                            </MenuItem>
-                        ))}
-                    </TextField>
-                    <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                        <KeyboardDatePicker
-                            margin="normal"
-                            id="paymentDate"
-                            name="paymentDate"
-                            label="Choose payment date"
-                            format="MM/dd/yyyy"
-                            value={paymentDate}
-                            onChange={paymentDateChange}
-                            KeyboardButtonProps={{
-                                'aria-label': 'change date',
-                            }}
-                            className={s.input}
-                        />
-                        <KeyboardDatePicker
-                            margin="normal"
-                            id="issueDate"
-                            name="issueDate"
-                            label="Choose issue date"
-                            format="MM/dd/yyyy"
-                            value={issueDate}
-                            onChange={issueDateChange}
-                            KeyboardButtonProps={{
-                                'aria-label': 'change date',
-                            }}
-                            className={s.input}
-                        />
-                    </MuiPickersUtilsProvider>
-                    <TextField
-                        id="status"
-                        name="status"
-                        select
-                        label="Status"
-                        value={formik.values.status}
-                        onChange={formik.handleChange}
-                        helperText="Please select your Product"
-                        variant="outlined"
-                        className={s.input}
-                    >
-                        {InvoicePaymentStatus.map((option, idx) => (
-                            <MenuItem key={idx} value={option}>
-                                {option}
-                            </MenuItem>
-                        ))}
-                    </TextField>
-                    <Button color="primary"  variant="contained" type="submit" className={s.btn}>
-                        Submit
-                    </Button>
+                    />
+                </MuiPickersUtilsProvider>
+                <TextField
+                    id="status"
+                    name="status"
+                    select
+                    label="Status"
+                    value={formik.values.status}
+                    onChange={formik.handleChange}
+                    placeholder="Please select your Product"
+                    variant="outlined"
+                    className={s.input}
+                    error={formik.touched.status && Boolean(formik.errors.status)}
+                    helperText={formik.touched.status && formik.errors.status}
+                >
+                    {InvoicePaymentStatus.map((option, idx) => (
+                        <MenuItem key={idx} value={option}>
+                            {option}
+                        </MenuItem>
+                    ))}
+                </TextField>
+                <Button color="primary" variant="contained" type="submit" className={s.btn}>
+                    Submit
+                </Button>
             </form>
         </Paper>
     );
